39ed19a305f8eb2c257fc6c30628009084e2e07d
SparkyDan555 <38540799+SparkyDan555@users.noreply.github.com>
2025-06-08 16:28:27 +0100

    fix: Use public API for light state update

--- components/sacn/sacn_light_effect.cpp
+++ components/sacn/sacn_light_effect.cpp
@@ -62,7 +62,7 @@ void SACNLightEffect::start() {
   
   // Force an immediate update of the light state
   this->state_->loop();
-  this->state_->output_->write_state(this->state_);  // Force hardware update
+  this->state_->apply_effect(); // Force immediate state update
 }
 
 void SACNLightEffect::stop() {
